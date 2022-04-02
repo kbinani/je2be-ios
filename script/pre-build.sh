0d9f112797bb5e3038e1414174385f137bea695e
kbinani <kbinani.bt@gmail.com>
2022-04-02 23:33:59 +0900

    Support all orientation types

---
+++ script/pre-build.sh
@@ -0,0 +1,11 @@
+set -ue
+
+PLISTBUDDY=/usr/libexec/PlistBuddy
+INFO_PLIST_PATH=CMakeFiles/je2be-ios.dir/Info.plist
+
+${PLISTBUDDY} -c "Delete :UISupportedInterfaceOrientations" "$INFO_PLIST_PATH"
+${PLISTBUDDY} -c "Add :UISupportedInterfaceOrientations array" "$INFO_PLIST_PATH"
+${PLISTBUDDY} -c "Add :UISupportedInterfaceOrientations:0 string UIInterfaceOrientationPortrait" "$INFO_PLIST_PATH"
+${PLISTBUDDY} -c "Add :UISupportedInterfaceOrientations:0 string UIInterfaceOrientationPortraitUpsideDown" "$INFO_PLIST_PATH"
+${PLISTBUDDY} -c "Add :UISupportedInterfaceOrientations:0 string UIInterfaceOrientationLandscapeLeft" "$INFO_PLIST_PATH"
+${PLISTBUDDY} -c "Add :UISupportedInterfaceOrientations:0 string UIInterfaceOrientationLandscapeRight" "$INFO_PLIST_PATH"
