e629dae29c3e13c383a48044f1a7328a987ad963
kbinani <kbinani.bt@gmail.com>
2022-04-03 21:33:38 +0900

    Add back button

--- src/ProgressViewController.swift
+++ src/ProgressViewController.swift
@@ -5,11 +5,14 @@ class ProgressViewController: UIViewController {
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var stepDescriptionLabel: UILabel!
     @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var exportButton: UIButton!
+    @IBOutlet weak var closeButton: UIButton!
     
     private let input: URL
     private let converter: Converter
     private let cancelRequested = AtomicBool(initial: false)
     private var progressSteps: [UIProgressView] = []
+    private var output: URL?
     
     init(input: URL, converter: Converter) {
         self.input = input
@@ -19,6 +22,12 @@ class ProgressViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        if let output = self.output {
+            try? FileManager.default.removeItem(at: output)
+        }
     }
     
     override func viewDidLoad() {
@@ -32,9 +41,17 @@ class ProgressViewController: UIViewController {
             progress.progress = 0
             self.stackView.addArrangedSubview(progress)
         }
-
+        
         self.cancelButton.setTitle(gettext("Cancel"), for: .normal)
         self.cancelButton.addTarget(self, action: #selector(cancelButtonDidTouchUpInside(sender:)), for: .touchUpInside)
+        
+        self.exportButton.setTitle(gettext("Export"), for: .normal)
+        self.exportButton.addTarget(self, action: #selector(exportButtonDidTouchUpInside(sender:)), for: .touchUpInside)
+        self.exportButton.isHidden = true
+        
+        self.closeButton.setTitle(gettext("Back"), for: .normal)
+        self.closeButton.addTarget(self, action: #selector(closeButtonDidTouchUpInside(sender:)), for: .touchUpInside)
+        self.closeButton.isEnabled = false
         
         DispatchQueue.global().async { [weak self] in
             guard let input = self?.input, let converter = self?.converter else {
@@ -47,6 +64,20 @@ class ProgressViewController: UIViewController {
     @objc func cancelButtonDidTouchUpInside(sender: AnyObject) {
         self.cancelButton.isEnabled = false
         self.cancelRequested.getAndSet(value: true)
+    }
+    
+    @objc func exportButtonDidTouchUpInside(sender: AnyObject) {
+        guard let output = self.output else {
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [output as Any], applicationActivities: nil)
+        vc.modalPresentationStyle = .popover
+        vc.popoverPresentationController?.sourceView = self.exportButton
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    @objc func closeButtonDidTouchUpInside(sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
 
@@ -65,22 +96,31 @@ extension ProgressViewController: ConverterDelegate {
         }
         return !cancelRequested.test()
     }
-
+    
     func converterDidFinishConversion(_ output: URL?) {
         DispatchQueue.main.async { [weak self] in
             guard let self = self else {
                 return
             }
+            
+            self.closeButton.isEnabled = true
+            
             if let output = output {
+                self.output = output
+                self.cancelButton.isHidden = true
+                self.exportButton.isHidden = false
+                
                 self.stepDescriptionLabel.text = gettext("Completed")
+
                 let vc = UIActivityViewController(activityItems: [output as Any], applicationActivities: nil)
                 vc.modalPresentationStyle = .popover
-                vc.popoverPresentationController?.sourceView = self.stepDescriptionLabel
-                vc.completionWithItemsHandler = { [weak self] (activityType, completed, returnedItems, activityError) in
-                    try? FileManager.default.removeItem(at: output)
-                    self?.dismiss(animated: true, completion: nil)
-                }
+                vc.popoverPresentationController?.sourceView = self.exportButton
                 self.present(vc, animated: true, completion: nil)
+            } else {
+                self.cancelButton.isHidden = true
+                self.exportButton.isHidden = true
+                
+                self.stepDescriptionLabel.text = gettext("Failed")
             }
         }
     }
