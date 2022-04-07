fd53e4e4eb358cb6ed60e8eb2f874a5ccdc1e2f2
kbinani <kbinani.bt@gmail.com>
2022-04-07 20:14:59 +0900

    Switch releaseStage by the CONFIGURATION

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -7,8 +7,14 @@ class AppDelegate: UIResponder {
 
 extension AppDelegate: UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        Bugsnag.start()
-
+        let config = BugsnagConfiguration.loadConfig()
+        if (Bundle.main.infoDictionary?["JE2BE_BUILD_CONFIGURATION"] as? String) == "Debug" {
+            config.releaseStage = "development"
+        } else {
+            config.releaseStage = "release"
+        }
+        Bugsnag.start(with: config)
+        
         let window = UIWindow(frame: UIScreen.main.bounds)
         self.window = window
         window.rootViewController = MainViewController()
