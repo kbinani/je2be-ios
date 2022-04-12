9e39f48e2a1ca7c7d3a1e459719f32e3ec90b98a
kbinani <kbinani.bt@gmail.com>
2022-04-12 22:56:12 +0900

    Layout error messages by myself

--- src/ErrorViewController.swift
+++ src/ErrorViewController.swift
@@ -2,24 +2,68 @@ import UIKit
 
 class ErrorViewController: UIViewController {
     
-    @IBOutlet weak var stackView: UIStackView!
-    
-    let messages: [String]
+    private weak var errorView: ErrorView!
+    private let insets = UIEdgeInsets(top: 40, left: 20, bottom: 15, right: 20)
+    private let messages: [String]
 
     init(messages: [String]) {
         self.messages = messages
-        super.init(nibName: "ErrorViewController", bundle: nil)
+        super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        self.messages = []
+        super.init(coder: coder)
     }
-    
+            
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let width: CGFloat = 375 - 40
-        var height: CGFloat = 80
+        let width: CGFloat = 375
+        let errorView = ErrorView(messages: self.messages, width: width - self.insets.left - self.insets.right)
+        self.errorView = errorView
+        self.view.addSubview(errorView)
+        
+        let size = errorView.intrinsicContentSize
+
+        self.preferredContentSize = CGSize(width: width, height: size.height + self.insets.top + self.insets.bottom)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.errorView.frame = CGRect(x: self.insets.left,
+                                      y: self.insets.top,
+                                      width: self.view.bounds.width - self.insets.left - self.insets.right,
+                                      height: self.view.bounds.height - self.insets.top - self.insets.bottom)
+    }
+}
+
+
+class ErrorView: UIView {
+    
+    private let messages: [String]
+    private let width: CGFloat
+    private var attributedString: NSAttributedString!
+    private var framesetter: CTFramesetter!
+    private var textFrame: CTFrame?
+    
+    init(messages: [String], width: CGFloat) {
+        self.messages = messages
+        self.width = width
+        super.init(frame: CGRect(x: 0, y: 0, width: self.width, height: 0))
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        self.messages = []
+        self.width = 1
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        self.isOpaque = false
+        
         let font = UIFont.systemFont(ofSize: 17)
         let style = NSMutableParagraphStyle()
         style.setParagraphStyle(NSParagraphStyle.default)
@@ -30,19 +74,48 @@ class ErrorViewController: UIViewController {
         let attributes: [NSAttributedString.Key: Any] = [
             .font: font,
             .paragraphStyle: style,
+            .foregroundColor: UIColor.black,
         ]
-
-        for message in messages {
-            let label = UILabel(frame: .zero)
-            label.numberOfLines = Int.max
-            let text = NSMutableAttributedString(string: bullet + " " + message, attributes: attributes)
-            label.attributedText = text
-            label.minimumScaleFactor = 1
-            self.stackView.addArrangedSubview(label)
-            let size = label.systemLayoutSizeFitting(.init(width: width, height: CGFloat.greatestFiniteMagnitude))
-            height += size.height
+        let lines = messages.map { message in
+            return bullet + " " + message
         }
-        
-        self.preferredContentSize = CGSize(width: width, height: height * 1.1)
+        let attributedString = NSAttributedString(string: lines.joined(separator: "\n"), attributes: attributes)
+        self.attributedString = attributedString
+        self.framesetter = CTFramesetterCreateWithAttributedString(attributedString)
+        self.textFrame = createFrame()
+    }
+    
+    private func createFrame() -> CTFrame {
+        let pathBox = self.bounds
+        let path = CGMutablePath()
+        path.addRect(pathBox)
+        return CTFramesetterCreateFrame(self.framesetter, CFRangeMake(0, self.attributedString.length), path, nil)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard let ctx = UIGraphicsGetCurrentContext(), let frame = self.textFrame else {
+            return
+        }
+        ctx.saveGState()
+        defer {
+            ctx.restoreGState()
+        }
+        ctx.clear(self.bounds)
+        ctx.translateBy(x: 0, y: self.bounds.height)
+        ctx.scaleBy(x: 1, y: -1)
+        CTFrameDraw(frame, ctx)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.textFrame = createFrame()
+        self.invalidateIntrinsicContentSize()
+        self.setNeedsDisplay()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let constraints = CGSize(width: self.width, height: CGFloat.greatestFiniteMagnitude)
+        let size = CTFramesetterSuggestFrameSizeWithConstraints(self.framesetter, CFRangeMake(0, self.attributedString.length), nil, constraints, nil)
+        return CGSize(width: self.width, height: size.height)
     }
 }
