7a2d7dae3ad6b1ae0e974ae29d5df19042478929
kbinani <kbinani.bt@gmail.com>
2025-11-08 22:32:10 +0900

    Migrate to UIScene

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -1,16 +1,5 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder {
-    var window: UIWindow?
-}
-
-extension AppDelegate: UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        self.window = window
-        window.rootViewController = MainViewController()
-        window.makeKeyAndVisible()
-        return true
-    }
+class AppDelegate: UIResponder, UIApplicationDelegate {
 }
---
+++ src/SceneDelegate.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class SceneDelegate: UIResponder {
+    var window: UIWindow?
+}
+
+extension SceneDelegate: UIWindowSceneDelegate {
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else {
+            return
+        }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = MainViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
